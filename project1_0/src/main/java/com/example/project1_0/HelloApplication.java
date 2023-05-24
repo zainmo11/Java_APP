2463c93c5f61046d1af8d333cca48c19817f9295
Abdelrahman Hamada <69439342+abdlrhman08@users.noreply.github.com>
2023-05-24 09:02:52 +0300

    Added basic Sceneloader and Bloc class

--- project1_0/src/main/java/com/example/project1_0/HelloApplication.java
+++ project1_0/src/main/java/com/example/project1_0/HelloApplication.java
@@ -10,8 +10,23 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
+	
+	private static Parent root;
     @Override
     public void start(Stage stage) throws IOException {
+		
+		/*
+		
+        root = (Parent)FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        SceneLoader loader = new SceneLoader("C:\\Users\\Abdelrahman\\Downloads\\Example.mdl", root);
+        loader.parseFile();
+        loader.drawScene();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 720.0, 640.0));
+        primaryStage.show();
+		*/
+		
+		
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         String iconPath = "file:///C:/Users/zyn66/Desktop/project1_0/My%20project-1%20(1).png";
