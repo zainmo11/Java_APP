06fa200d691f2b12b60b5f3f349a13f59ca9d85e
Abdelrahman Hamada <69439342+abdlrhman08@users.noreply.github.com>
2023-05-25 07:43:26 +0300

    Added HTML Viewer

--- project1_0/src/main/java/com/example/project1_0/Line.java
+++ project1_0/src/main/java/com/example/project1_0/Line.java
@@ -3,15 +3,23 @@ package com.example.project1_0;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import java.util.ArrayList;
+
 public class Line {
 
-    private int start;
-    public Line(NodeList properties) {
+    private double start;
+    public Line(NodeList properties, ArrayList<Object> Blocks) {
         for (int i = 0; i < properties.getLength(); i++) {
             Element currentElement = (Element)properties.item(i);
 
             if (currentElement.getAttribute("Name").equals("Src")) {
                 int source = currentElement.getTextContent().charAt(0) - 48;
+
+                for (Object block : Blocks) {
+                    if (((Block)block).getID() == source ) {
+                        start = ((Block)block).getX();
+                    }
+                }
             }
 
             if (currentElement.getParentNode().getNodeName().equals("Branch")) {
